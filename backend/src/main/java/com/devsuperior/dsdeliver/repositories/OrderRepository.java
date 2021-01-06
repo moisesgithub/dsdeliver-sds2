1da410249ad67588dc762c374e3e0ce3e06570d1
unknown <moises.github@outlook.com>
2021-01-05 23:07:40 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -10,7 +10,14 @@ import com.devsuperior.dsdeliver.entities.Order;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	
 	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
-	 + " WHERE obj.status = 0 ORDER obj.moment ASC")
+	 + " WHERE obj.status = 0 ORDER BY obj.moment ASC")
 	List<Order> findOrdersWithProducts();
-
 }
+
+/* "O meu código está dando esse erro! não sei o que fazer!"
+
+Error starting ApplicationContext. 
+To display the conditions report re-run your application with 'debug' enabled.
+2021-01-05 23:00:31.916 ERROR 3232 --- [main] o.s.boot.SpringApplication               
+: Application run failed
+*/
