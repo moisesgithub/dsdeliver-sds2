120df209704087b7c25c9fdfcadeb2a1326c22df
unknown <moises.github@outlook.com>
2021-01-06 21:30:29 -0300

    GET /orders3ok

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -34,7 +34,7 @@ public class Order implements Serializable {
 	           joinColumns = @JoinColumn(name = "order_id"),
 	           inverseJoinColumns = @JoinColumn(name = "product_id"))
 	
-	private Set <Product> Products = new HashSet<>();
+	private Set <Product> products = new HashSet<>();
 	
 	public Order() {
     }
@@ -98,7 +98,7 @@ public class Order implements Serializable {
 	}
 
 	public Set<Product> getProducts() {
-		return Products;
+		return products;
 	}
 
 	@Override
