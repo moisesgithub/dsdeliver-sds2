55221bdaae9f3b7023c1563b5b4ebdaf5688213a
moisesgithub <70031129+moisesgithub@users.noreply.github.com>
2021-01-09 08:57:23 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Navibar from './Navbar';
+import Navbar from './Navbar'
 function App() {
   return (
   <div>
-<h1>Hello!</h1>
+    <Navbar />
   </div>
   );
 }
---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import App from "../App";
+
+import './styles.css';
+import {ReactComponent as Logo} from './logo.svg';
+function Navbar(){
+    return(
+        <nav className = "main-navbar">
+          <Logo />
+          <a href = "home" className = "logo-text"> DS Delivery </a>
+        </nav>
+    )
+}
+export default Navbar;
