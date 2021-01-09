232e78ba584f27afd8801f5d7c19866cd0acffaa
moisesgithub <70031129+moisesgithub@users.noreply.github.com>
2021-01-09 10:01:52 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
-import Navbar from './Navbar'
+import Home from './Home';
+import Navbar from './Navbar';
+
 function App() {
   return (
-  <div>
+    <>
     <Navbar />
-  </div>
+    <Home />
+    </>
   );
 }
 export default App;
