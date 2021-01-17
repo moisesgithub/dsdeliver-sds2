8ea9686056a14dba43c176ed6c08feec718fa1d0
moisesgithub <70031129+moisesgithub@users.noreply.github.com>
2021-01-17 12:14:21 -0300

    Finalizado front-mobile

--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 import Orders from "./Orders";
+import OrderDetails from "./OrderDetails";
 import Home from "./Home";
 
 const Stack = createStackNavigator();
@@ -19,6 +20,7 @@ function Routes(){
          >
              <Stack.Screen name= "Home" component= {Home}></Stack.Screen>
              <Stack.Screen name= "Orders" component= {Orders}></Stack.Screen>
+             <Stack.Screen name= "OrderDetails" component= {OrderDetails}></Stack.Screen>
              </Stack.Navigator> 
       </NavigationContainer>
   )
---
+++ front-mobile/src/api.tsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_URL = 'https://moises-sds2.herokuapp.com';
+
+export function fectchOrders(){
+    return axios(`${API_URL}/orders`);
+}
+export function confirmDelivery (orderId: number){
+    return axios.put(`${API_URL}/orders/${orderId}/delivered`);
+}
